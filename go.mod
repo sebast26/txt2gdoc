00344848115a1f568727de42e0b14050b0c4a672
Sebastian Górecki <sebastian.gorecki@olx.pl>
2022-11-09 23:22:16 +0100

    implements creating oauth_client

--- go.mod
+++ go.mod
@@ -5,10 +5,16 @@ go 1.19
 require (
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.8.1
+	golang.org/x/oauth2 v0.2.0
 )
 
 require (
+	cloud.google.com/go/compute/metadata v0.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/net v0.2.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/google/oauth_client.go
+++ internal/google/oauth_client.go
@@ -1,7 +1,12 @@
 package google
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/pkg/errors"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 	"net/http"
 	"os"
 )
@@ -10,11 +15,75 @@ type OAuthClient struct {
 	httpClient *http.Client
 }
 
-func NewOAuthClient(credentialsFilePath string) (*OAuthClient, error) {
-	_, err := os.ReadFile(credentialsFilePath)
+// NewOAuthClient creates new OAuthClient that can be used to create Google services.
+func NewOAuthClient(credentialsFilePath, tokenFilePath string) (*OAuthClient, error) {
+	b, err := os.ReadFile(credentialsFilePath)
 	if err != nil {
 		return nil, errors.Wrap(err, "reading client secret file failed")
 	}
 
-	return &OAuthClient{}, nil
+	config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/documents")
+	if err != nil {
+		return nil, errors.Wrap(err, "parsing client secret file to config failed")
+	}
+
+	tok, err := tokenFromFile(tokenFilePath)
+	if err != nil {
+		tok, err = getTokenFromWeb(config)
+		if err != nil {
+			return nil, err
+		}
+		err = saveToken(tokenFilePath, tok)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &OAuthClient{httpClient: config.Client(context.Background(), tok)}, nil
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	defer func() {
+		_ = f.Close()
+	}()
+	if err != nil {
+		return nil, err
+	}
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Requests a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) (*oauth2.Token, error) {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		return nil, errors.Wrap(err, "failed to read authorization code")
+	}
+
+	ctx := context.Background()
+	tok, err := config.Exchange(ctx, authCode)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to retrieve token from web")
+	}
+	return tok, nil
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) error {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	defer func() {
+		_ = f.Close()
+	}()
+	if err != nil {
+		return errors.Wrap(err, "failed to cache OAuth token")
+	}
+	return json.NewEncoder(f).Encode(token)
 }
